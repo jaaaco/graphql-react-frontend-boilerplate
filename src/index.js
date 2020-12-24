9c10f768a15bd4892f399ffcc1df2e9107bf932b
Jakub Wietrzyk <jakub@wietrzyk.com>
2020-12-24 11:57:21 +0100

    Apollo client setup & semantic ui installation

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+    uri: 'http://localhost:4000',
+    cache: new InMemoryCache()
+});
 
 ReactDOM.render(
   <React.StrictMode>
